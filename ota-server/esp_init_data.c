8a652d3ee2a5079fffc7152e79bad3f4beff757c
Victor Marcelino <victor.fmarcelino@gmail.com>
2020-04-03 06:43:18 -0300

    better initialization detection

--- ota-server/esp_init_data.c
+++ ota-server/esp_init_data.c
@@ -30,6 +30,7 @@
 //#include "esp_mphal.h"
 #include "user_interface.h"
 //#include "extmod/misc.h"
+#include "spi_flash.h"
 
 #define NORETURN __attribute__((noreturn))
 NORETURN void call_user_start(void);
@@ -48,103 +49,32 @@ static const uint8_t default_init_data[] __attribute__((aligned(4))) = {
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
     0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 
-// void pm(uint8_t *mem)
-// {
-
-//     //print X label
-//     ets_printf("         ");
-//     for (int i = 1; i <= 16; i++)
-//     {
-//         ets_printf("%2d ", i);
-//     }
-//     ets_printf("\n\n ");
-
-//     // print Y label and items
-//     for (int i = 0; i < 8; i++)
-//     {
-//         ets_printf("%d(%3d)  ", i + 1, i * 16);
-//         for (int j = 0; j < 16; j++)
-//         {
-//             const int l = i * 16 + j;
-//             ets_printf("%02x ", mem[l]);
-//         }
-//         ets_printf("\n ");
-//     }
-// }
-// void print_mem_comp(uint8_t *mem, uint8_t *mem2)
-// {
-//     if (mem == NULL)
-//         mem = default_init_data;
-
-//     //print X label
-//     ets_printf("         ");
-//     for (int i = 1; i <= 16; i++)
-//     {
-//         ets_printf("%2d ", i);
-//     }
-//     ets_printf("\n\n ");
-
-//     // print Y label and items
-//     for (int i = 0; i < 8; i++)
-//     {
-//         ets_printf("%d(%3d)  ", i + 1, i * 16);
-//         for (int j = 0; j < 16; j++)
-//         {
-//             const int l = i * 16 + j;
-//             if (mem2 == NULL)
-//                 ets_printf("%02x ", mem[l]);
-
-//             else
-//                 ets_printf("%s  ", mem[l] == mem2[l] ? "." : "#");
-//         }
-//         ets_printf("\n ");
-//     }
-// }
-
-// void print_mem_s(uint8_t *mem, uint32_t start)
-// {
-//     ets_printf("offset: %x", start);
-//     SPIRead(start, mem, sizeof(mem));
-//     print_mem_comp(mem, NULL);
-//     ets_printf("\n\n\n");
-//     print_mem_comp(NULL, mem);
-//     ets_printf("\n\n\n");
-// }
-// void print_mem(uint8_t *mem)
-// {
-//     print_mem_s(mem, esp_init_data_start);
-// }
-
 void firmware_start(void)
 {
-    // For SDK 1.5.2, either address has shifted and not mirrored in
-    // eagle.rom.addr.v6.ld, or extra initial member was added.
-    //ets_printf("             test: 0x%x\n", testval);
-    //ets_printf("       flash chip: 0x%x\n", flashchip);
-    //SpiFlashChip *flash = (SpiFlashChip *)(flashchip);
-
     ets_printf("OTA firmware\n");
 
     ets_printf("chip size (Bytes): %u\n", flashchip->chip_size);
 
-    // the init data must be at the last 16KB of the flash
-
-    //char buf[128];
-
     bool inited = false;
-    // for (int i = 0; i < sizeof(buf); i++)
-    // {
-    //     if (buf[i] != 0xff)
-    //     {
-    //         inited = true;
-    //         break;
-    //     }
-    // }
+    // the init data must be at the last 16KB of the flash
+    char buf[sizeof(default_init_data)];
+    SPIRead(esp_init_data_start, buf, sizeof(default_init_data));
+    if (buf[0] != 0x05)
+        inited = false;
+    else
+        for (int i = 0; i < sizeof(buf); i++)
+        {
+            if (buf[i] != 0xff)
+            {
+                inited = true;
+                break;
+            }
+        }
 
     if (!inited)
     {
         ets_printf("Writing init data\n");
-        //SPIRead((uint32_t)&default_init_data - 0x40200000, buf, sizeof(buf));
+        SPIEraseSector(esp_init_data_start / SPI_FLASH_SEC_SIZE);
         SPIWrite(esp_init_data_start, default_init_data, sizeof(default_init_data));
         ets_printf("Done writing\n");
     }
